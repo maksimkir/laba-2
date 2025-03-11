29feae0ad816dedac586601e09543fc400d6ebb5
maksimkir <kiriluk157@gmail.com>
2025-03-11 19:08:18 +0200

    add class Employee

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Employee{
+protected:
+    int id;
+    double salary;
+public:
+    Employee(int id, int salary):id(id), salary(salary) {}
+    void showInfo() const {
+        cout << "ID: " << id << endl<< "Salary: "<< salary << endl;
+    }
+    double calculate() const {
+        return salary;
+    }
+};
